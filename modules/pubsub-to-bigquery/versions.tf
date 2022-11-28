e75f2dddd01347c9d78e8ae2fe5b60e37c54ab47
Dinko Osrecki <dinko.osrecki@emarsys.com>
2022-11-28 11:39:40 +0100

    fix: support any google version >= 4

--- modules/pubsub-to-bigquery/versions.tf
+++ modules/pubsub-to-bigquery/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 1.0.0, < 2.0.0"
 
   required_providers {
-    google      = "~> 4.40.0"
-    google-beta = "~> 4.40.0"
+    google      = ">= 4"
+    google-beta = ">= 4"
   }
 }
